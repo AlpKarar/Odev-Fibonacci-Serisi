38454c05065b98b3d123a49d4995224de4aadadc
Alp Karar <88462979+AlpKarar@users.noreply.github.com>
2022-02-15 21:26:27 +0300

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,31 @@
+/*
+  Ödev
+  Java döngüler ile fibonacci serisi bulan program yazıyoruz. Fibonacci serisinin eleman sayısını kullanıcıdan alın.
+*/
+
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        int len;
+        
+        System.out.print("The length of Fibonacci Series: ");
+        len = input.nextInt();
+        
+        int num1 = 0, num2 = 1, temp;
+        System.out.print(num1 + " " + num2 + " ");
+        
+        for (int i=0; i<len-2; i++) {
+            temp = num2;
+            num2 += num1;
+            num1 = temp;
+            System.out.print(num2 + " ");
+        }
+        
+    }
+}
